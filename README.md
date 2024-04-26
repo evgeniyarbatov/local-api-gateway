835383adef5f43aa429bd2efa87ae9ce468a9f8b
Evgeny Arbatov <arbatovevgeny@gmail.com>
2024-04-26 12:36:54 +0800

    API GW code

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # local-api-gateway
+
 Make API gateway which can handle localhost going offline
+
+## Run
+
+Start RabbitMQ:
+
+```
+docker compose up
+```
+
+List queues:
+
+```
+docker exec -it rabbitmq rabbitmqctl list_queues
+```
---
+++ gateway/gateway.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const amqp = require('amqplib');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 8080;
+
+// Middleware to parse JSON bodies
+app.use(bodyParser.json());
+
+async function sendMessage(
+  api,
+  params,
+) {
+  try {
+    const connection = await amqp.connect('amqp://localhost');
+    const channel = await connection.createChannel();
+
+    await channel.assertQueue(api, { durable: false });
+    channel.sendToQueue(api, Buffer.from(JSON.stringify(params)));
+
+    console.log(`Queued api: ${api} params: ${JSON.stringify(params)}`);
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
+
+app.post('/api', async (req, res) => {
+  if (
+    !req.body || 
+    !req.body.api ||
+    !req.body.params
+  ) {
+    return res.status(400).json({
+      error: 'Required field is missing'
+    });
+  }
+
+  await sendMessage(
+    req.body.api,
+    req.body.params,
+  );
+
+  res.json({ 
+    success: true, 
+    message: 'Submitted'
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
